b55f391f574008c9238773a09288e6328bc54099
honglikehk <christine.hong.to@gmail.com>
2019-06-19 14:43:37 -0400

    added some javascript - array and defined some variables

---
+++ javascript/game.js
@@ -0,0 +1,29 @@
+    // Array of players 
+    var players = [
+    "leonard",
+    "lowry",
+    "lin",
+    "vanvleet",
+    "gasol",
+    "siakam",
+    "ibaka",
+    "green",
+    "anundby", 
+    "mccaw", 
+    "powell", 
+    "meeks", 
+    "moreland", 
+    "loyd", 
+    "miller"
+    ];
+    // items in the array 
+    var arrayPlayers = players.length; 
+    //message 
+    var msg = '';
+    // Counter 
+    var i;
+    // Creating variables to hold wins and number of guesses
+    var wins = 0;
+    var guesses = 20;
+
+    
