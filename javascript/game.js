c0cc6505daadcc3e5280522cb1393ebd4bae3cfb
honglikehk <christine.hong.to@gmail.com>
2019-07-10 13:53:56 -0400

    debugging

--- javascript/game.js
+++ javascript/game.js
@@ -34,6 +34,11 @@ function randomPlayer() {
 }
 
 var word = randomPlayer();
+
+// the next word in the game
+var nextWord = randomPlayer();
+console.log(nextWord);
+
 console.log(word);
 
 var remainingLetters = word.length;
@@ -72,36 +77,49 @@ document.onkeyup = function(event) {
 
   console.log(getGuess());
 
-  var comparePlayer = word.split("");
-  var indPlayer = comparePlayer.indexOf(guess);
+  function updateGameState(word, guess, answerArray) {
+    var comparePlayer = word.split("");
+    var indPlayer = comparePlayer.indexOf(guess);
 
-  console.log(indPlayer);
-  console.log(answerArray);
+    console.log(indPlayer);
+    console.log(answerArray);
 
-  for (var j = 0; j < word.length; j++) {
-    if (indPlayer === -1) {
-      guessedLetters.push(guess);
-      alreadyGuessed.innerHTML = guesses;
-      guesses--;
-    } else {
-      if (word[j] === guess) {
-        answerArray[j] = guess;
-        currentWord.innerHTML = answerArray;
-        remainingLetters--;
+    for (var j = 0; j < word.length; j++) {
+      if (indPlayer === -1) {
+        guessedLetters.push(guess);
+        alreadyGuessed.innerHTML = guesses;
+        guesses--;
+      } else {
+        if (word[j] === guess) {
+          answerArray[j] = guess;
+          currentWord.innerHTML = answerArray;
+          remainingLetters--;
+        }
       }
-    }
 
-    //3. if the letter clicked is correct then go fill all the underscores with the correct answer and show the progress to the user
+      //3. if the letter clicked is correct then go fill all the underscores with the correct answer and show the progress to the user
+    }
   }
 
-  //4. if it is not correct then log it to an empty array adn show it
+  updateGameState(word, guess, answerArray);
+  console.log(updateGameState(word, guess, answerArray));
+  //4. if it is not correct then log it to an empty array and show it
 
   //5.  when the word is answered correctly --- add 1 point to the wins
   // when the wins --- went through the length of the array - the user wins the game
   // when they just win one word - reset .. the word and give a new random word
-  if (remainingLetters === 0) {
-    playerSetupUnderScores(word);
-  }
 
   // 6. when the remaining guesses = 0; game over for the user
 };
+
+// things still need to be done
+/*
+1. log the number of wins - when all the array items - have been played with - then the player wins
+
+2. deduct from the number of guesses and log it in the guess panel 
+
+3. ***** SWITCH word when completed to the next item in the array 
+
+4. if the user # of guesses reaches 0, then its game over for the user. 
+
+*/
